fb6d393a7f1568a8096d6e49faf5a35dc3483f74
devstra <oscar.f@outlook.fr>
2021-06-05 01:53:46 +0200

    finished start button. just need to display results

--- src/main/java/fr/devsta/jfxtest/PrimaryController.java
+++ src/main/java/fr/devsta/jfxtest/PrimaryController.java
@@ -1,12 +1,22 @@
 package fr.devsta.jfxtest;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.MalformedURLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import fr.unistra.pelican.algorithms.io.ImageLoader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
@@ -14,13 +24,13 @@ import javafx.stage.FileChooser;
 public class PrimaryController {
 
   @FXML
-  private RadioButton rgbRadio, hsvRadio;
-  @FXML
   private ImageView selectedImage;
   @FXML
   private Label filenameLabel;
-
+  @FXML
+  private ToggleGroup searchMode;
   private String imageFile;
+
 
   public void pickImage(ActionEvent actionEvent) throws MalformedURLException {
 
@@ -41,22 +51,116 @@ public class PrimaryController {
     } else {
       filenameLabel.setText("Image file selection cancelled.");
     }
-
-  }
-
-  private void switchToSecondary() throws IOException {
-    App.setRoot("secondary");
   }
 
 
-  public void getSelectedMode(ActionEvent evt) {
-    // tg.getSelectedToggle()
-    if (rgbRadio.isSelected()) {
-      System.out.println(rgbRadio.getText());
+  /**
+   * Creates a connection to the specified SQLite database and returns it.
+   * 
+   * @param pathToDatabase path to .db file
+   * @return connection to SQLite database
+   * @throws SQLException
+   */
+  public static Connection getDatabase(String pathToDatabase) throws SQLException {
+    // /Users/oscar/Documents/LPIOT/ImageJava/imagesearch/images.db
+    String url = "jdbc:sqlite:" + pathToDatabase;
+    Connection conn = DriverManager.getConnection(url);
+    System.out.println("Connection to SQLite database has been established.");
+    return conn;
+  }
+
+
+  public void startSearch(ActionEvent evt) {
+    RadioButton selectedRadioButton = (RadioButton) searchMode.getSelectedToggle();
+
+    if (selectedRadioButton.getText() == "RGB") {
+      System.out.println("RGB is selected");
+    } else {
+      System.out.println("HSV is selected");
     }
 
-    if (hsvRadio.isSelected()) {
-      System.out.println(hsvRadio.getText());
+    String imgPath = imageFile.split("file:")[1];
+    System.out.println(imgPath);
+
+    try {
+      Connection connection =
+          getDatabase("/Users/oscar/Documents/LPIOT/ImageJava/imagesearch/images.db");
+      fr.unistra.pelican.Image testImage = ImageLoader.exec(imgPath);
+      // // addImagesToDatabase("/Users/oscar/Documents/LPIOT/ImageJava/imagesearch/images",
+      // // connection);
+      //
+      TreeMap<Double, String> images =
+          findSimilarImages(testImage, connection, selectedRadioButton.getText());
+
+      for (Entry<Double, String> entry : images.entrySet()) {
+        System.out.println(entry.getKey() + " : " + entry.getValue());
+      }
+    } catch (SQLException e) {
+      System.err.println("Some error happenned");
     }
   }
+
+  /**
+   * Returns the 10 most similar images in the database.
+   * 
+   * @param testImage base image to compare to
+   * @param conn SQLite database connection
+   * @return
+   * @throws SQLException
+   */
+  public static TreeMap<Double, String> findSimilarImages(fr.unistra.pelican.Image testImage,
+      Connection conn, String mode) throws SQLException {
+
+    TreeMap<Double, String> images = new TreeMap<>();
+    Gson gson = new GsonBuilder().create();
+    double[][] optimizedHistogram = null;
+
+    // Step 1: Apply filters to request image & get optimized histogram
+    fr.unistra.pelican.Image newRequestImage = Utils.applyMedianFilter(testImage);
+
+    switch (mode) {
+      case "RGB":
+        double[][] requestHistogram = Utils.getRgbHistogram(newRequestImage);
+        double[][] discreteHistogram = Utils.getDiscreteHistogram(requestHistogram);
+        optimizedHistogram = Utils.getNormalisedRgbHistogram(discreteHistogram,
+            (newRequestImage.getXDim() * newRequestImage.getYDim()));
+        break;
+      case "HSV":
+        double[][] requestHistogramHsv = Utils.getHsvHistogram(newRequestImage);
+        double[][] discreteHistogramHsv = Utils.getDiscreteHsvHistogram(requestHistogramHsv);
+        optimizedHistogram = Utils.getNormalisedRgbHistogram(discreteHistogramHsv,
+            (newRequestImage.getXDim() * newRequestImage.getYDim()));
+        break;
+    }
+
+
+    // Step 2: Fetch images from the database & calculate their similarity with the request image
+    String sql = "SELECT * FROM images";
+    Statement stmt = conn.createStatement();
+    ResultSet rs = stmt.executeQuery(sql);
+
+
+    switch (mode) {
+      case "RGB":
+        while (rs.next()) {
+          double[][] resultHistogram = gson.fromJson(rs.getString("histogram"), double[][].class);
+          double similarityRatio = Utils.getRgbSimilarity(optimizedHistogram, resultHistogram);
+          images.put(similarityRatio, rs.getString("filename"));
+        }
+        break;
+      case "HSV":
+        while (rs.next()) {
+          double[][] resultHistogram =
+              gson.fromJson(rs.getString("hsvHistogram"), double[][].class);
+          double similarityRatio = Utils.getRgbSimilarity(optimizedHistogram, resultHistogram);
+          images.put(similarityRatio, rs.getString("filename"));
+        }
+        break;
+    }
+
+
+    // TODO: return on first 10 entries
+    return images;
+  }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module fr.devsta.jfxtest {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires pelican;
+    requires java.sql;
+    requires com.google.gson;
 
     opens fr.devsta.jfxtest to javafx.fxml;
     exports fr.devsta.jfxtest;
